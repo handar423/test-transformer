5d875138be9d7c42f9ad2894afc851d4b67cd26b
Zhen Zhang <zhangzhen.dev@gmail.com>
2021-05-01 13:04:25 +0000

    debug

--- examples/text-classification/run_tf_glue.py
+++ examples/text-classification/run_tf_glue.py
@@ -35,6 +35,12 @@ try:
 except:
     hvd.init()
 
+gpus = tf.config.experimental.list_physical_devices('GPU')
+for gpu in gpus:
+    tf.config.experimental.set_memory_growth(gpu, True)
+if gpus:
+    tf.config.experimental.set_visible_devices(gpus[hvd.local_rank()], 'GPU')
+
 class Split(Enum):
     train = "train"
     dev = "validation"
